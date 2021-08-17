83c789a1199429498c6d9a109295488e68410697
maohongyun <maohongyun@bytedance.com>
2021-08-17 20:16:59 +0800

    添加option支持不编译，只安装

--- compile_dy.sh
+++ compile_dy.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-my_func(){
+install(){
+	adb install ./app/build/outputs/apk/douyinCn/debug/app-douyin-cn-debug.apk;
+	date;
+}
+
+restart(){
+	adb shell am force-stop com.ss.android.ugc.aweme;
+	adb shell am start -n com.ss.android.ugc.aweme/.splash.SplashActivity;
+	date;
+}
+
+compile(){
 	./gradlew :app:assembleDouyinCnDebug;
 	if [ $? -eq 0 ]; then
 		echo "compile succeed"
@@ -8,10 +19,48 @@ my_func(){
 		return
 	fi
 	date;
-	adb install ./app/build/outputs/apk/douyinCn/debug/app-douyin-cn-debug.apk;
-	adb shell am force-stop com.ss.android.ugc.aweme;
-	adb shell am start -n com.ss.android.ugc.aweme/.splash.SplashActivity;
-	date;
 }
 
-my_func
+unset compile_dy_options
+unset compile_dy_options_default
+compile_dy_options="default"
+compile_dy_options_default="default"
+while getopts ":n:a:h:i" optname
+do
+    case "$optname" in
+      "n")
+        echo "get option -n,value is $OPTARG"
+        ;;
+      "a")
+        echo "get option -a ,value is $OPTARG"
+        ;;
+      "h")
+        echo "get option -h,eg:./test.sh -n 编程珠玑 -a 守望先生"
+        ;;
+	  "i")
+	  	echo "will not compile project"
+		compile_dy_options="i"
+		install;
+		restart;
+		;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+	  "")
+	    echo 'nothing'
+		;;
+      *)
+        echo "Unknown error while processing options"
+        ;;
+    esac
+    # echo "option index is $OPTIND"
+done
+
+if [ $compile_dy_options == $compile_dy_options_default ]; then 
+	compile;
+	install;
+	restart;
+fi
